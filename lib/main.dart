cdf09161502c7be52d2be59588849ceaa246ead1
vladoja <dr.jalo2@gmail.com>
2023-10-04 16:13:23 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Action Manager POC"),
+        ),
+        body: Center(
+          child: const Text("Welcome"),
+        ),
+      ),
+    );
+  }
+}
