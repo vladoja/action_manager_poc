1170bd79e3ac4220b351d834359798288da3a463
vladoja <dr.jalo2@gmail.com>
2023-10-06 09:05:09 +0200

    Domain: Person repository

---
+++ lib/features/app/domain/repositories/person_repository.dart
@@ -0,0 +1,6 @@
+import 'package:action_manager_poc/core/resources/data_state.dart';
+import 'package:action_manager_poc/features/app/domain/entities/person.dart';
+
+abstract class PersonRepository {
+  Future<DataState<List<PersonEntity>>> getPersons();
+}
