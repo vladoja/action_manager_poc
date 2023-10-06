337f3b1832c19fe237bd7bf8532d1ef035fe2b7d
vladoja <dr.jalo2@gmail.com>
2023-10-06 10:56:57 +0200

    Person repository impl

---
+++ lib/features/app/data/repositories/person_repository_impl.dart
@@ -0,0 +1,20 @@
+import 'package:action_manager_poc/features/app/data/data_sources/local/app_database.dart';
+import 'package:action_manager_poc/features/app/domain/entities/person.dart';
+import 'package:action_manager_poc/core/resources/data_state.dart';
+import 'package:action_manager_poc/features/app/domain/repositories/person_repository.dart';
+
+class PersonRepositoryImpl extends PersonRepository {
+  final AppDatabase _appDatabase;
+
+  PersonRepositoryImpl(this._appDatabase);
+
+  @override
+  Future<DataState<List<PersonEntity>>> getPersons() async {
+    try {
+      List<PersonEntity> persons = await _appDatabase.personDao.getPersons();
+      return DataSuccess(persons);
+    } on Exception catch (e) {
+      return DataFailed(e);
+    }
+  }
+}
