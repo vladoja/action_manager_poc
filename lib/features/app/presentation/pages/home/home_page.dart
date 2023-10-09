a00593ed71ff8dac5a4b5da3ab1552525cc7ab94
vladoja <dr.jalo2@gmail.com>
2023-10-10 00:02:02 +0200

    Link to personal

--- lib/features/app/presentation/pages/home/home_page.dart
+++ lib/features/app/presentation/pages/home/home_page.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -10,7 +7,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _buildAppbar(context),
-      body: _buildBoddy(context),
+      body: _buildBody(context),
     );
   }
 
@@ -23,9 +20,21 @@ class HomePage extends StatelessWidget {
     );
   }
 
-  _buildBoddy(BuildContext context) {
-    return const Center(
-      child: Text("Welcome To Homepage"),
-    );
+  _buildBody(BuildContext context) {
+    return Center(
+        child: Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        InkWell(
+          child: const Text('Personal'),
+          onTap: () => _tapOnPersonal(context),
+        )
+      ],
+    ));
+  }
+
+  _tapOnPersonal(BuildContext context) {
+    Navigator.pushNamed(context, '/Personal');
   }
 }
