ce8112ed7ed64552fcf7fd96887eaafc03ffd285
vladoja <dr.jalo2@gmail.com>
2023-10-13 09:35:35 +0200

    Actions DataTable create DataRows

--- lib/features/app/presentation/pages/akcie/widgets/action_table_widget.dart
+++ lib/features/app/presentation/pages/akcie/widgets/action_table_widget.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:action_manager_poc/features/app/domain/entities/action.dart';
 import 'package:action_manager_poc/features/app/presentation/widgets/table_widget.dart';
 import 'package:data_table_2/data_table_2.dart';
@@ -28,15 +30,59 @@ class ActionTableWidget extends StatelessWidget {
       // numeric: true,
       size: ColumnSize.L,
     ),
-    DataColumn2(
-      label: Text('Vytvoril'),
-      // numeric: true,
-      size: ColumnSize.L,
-    ),
+    // DataColumn2(
+    //   label: Text('Vytvoril'),
+    //   // numeric: true,
+    //   size: ColumnSize.L,
+    // ),
   ];
+
+  final List<String> columnValues = [
+    "name",
+    "actionDate",
+    "licenceEvent",
+    "licenceType",
+    "licenceCourse",
+    // "created_by"
+  ];
+
+  void handleClickOnRow(int rowIndex) {
+    log("Row with id: $rowIndex clicked");
+  }
+
+  List<DataRow> createTableRows(List<ActionEntity> actions) {
+    List<DataRow> tableRows = [];
+    for (var i = 0; i < actions.length; i++) {
+      log(actions[i].toJson().toString());
+      tableRows.add(
+          ActionTableRowMapper.actionEntityToRow(actions[i], columnValues, (b) {
+        handleClickOnRow(i);
+      }));
+    }
+    return tableRows;
+  }
 
   @override
   Widget build(BuildContext context) {
+    tableRows.addAll(createTableRows(actions));
     return TableWidget(columns: tableColumns, rows: tableRows);
+  }
+}
+
+class ActionTableRowMapper {
+  static DataRow actionEntityToRow(
+      ActionEntity model, List<String> columns, void Function(bool?)? onTap) {
+    List<DataCell> cells = List<DataCell>.empty(growable: true);
+    Map<String, dynamic> actionJSON = model.toJson();
+    for (var columnValue in columns) {
+      if (actionJSON.containsKey(columnValue) == false) {
+        throw "ActionEntity object doesnt contains key:'$columnValue'";
+      }
+      cells.add(DataCell(Text(actionJSON[columnValue])));
+    }
+
+    DataRow row =
+        DataRow(onSelectChanged: (onTap != null) ? onTap : null, cells: cells);
+    return row;
   }
 }
