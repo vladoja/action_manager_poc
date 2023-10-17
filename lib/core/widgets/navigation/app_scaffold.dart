8108278df7517178c66573975edfa21f710ae9c7
vladoja <dr.jalo2@gmail.com>
2023-10-17 17:15:42 +0200

    AppScaffold

--- lib/core/widgets/navigation/app_scaffold.dart
+++ lib/core/widgets/navigation/app_scaffold.dart
@@ -3,6 +3,49 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:flutter_adaptive_scaffold/flutter_adaptive_scaffold.dart';
 
+class AppScaffold extends StatelessWidget {
+  final Widget body;
+  final Widget? secondaryBody;
+
+  const AppScaffold({
+    Key? key,
+    required this.body,
+    this.secondaryBody,
+  }) : super(key: key ?? const ValueKey('ScaffoldWithNestedNavigation'));
+
+  @override
+  Widget build(BuildContext context) {
+    return AdaptiveLayout(
+      body: SlotLayout(
+        config: <Breakpoint, SlotLayoutConfig>{
+          Breakpoints.small: SlotLayout.from(
+            key: const Key('Body Small'),
+            builder: (_) => secondaryBody != null ? secondaryBody! : body,
+          ),
+          Breakpoints.mediumAndUp: SlotLayout.from(
+            key: const Key('Body Medium'),
+            builder: (_) => body,
+          )
+        },
+      ),
+      secondaryBody: SlotLayout(
+        config: <Breakpoint, SlotLayoutConfig>{
+          Breakpoints.small: SlotLayout.from(
+            key: const Key('Body Small'),
+            builder: null,
+          ),
+          Breakpoints.mediumAndUp: SlotLayout.from(
+            key: const Key('Body Medium'),
+            builder: secondaryBody != null
+                ? (_) => secondaryBody!
+                : AdaptiveScaffold.emptyBuilder,
+          )
+        },
+      ),
+    );
+  }
+}
+
 class AppScaffoldShell extends StatelessWidget {
   final StatefulNavigationShell navigationShell;
   const AppScaffoldShell({
