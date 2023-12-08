dc68d8b4c1a17b20d04c523bd5a76a1ed87984dd
vladoja <dr.jalo2@gmail.com>
2023-12-08 16:41:06 +0100

    PersonalTableWidget, personEntityToRow()

--- lib/features/app/presentation/pages/personal/widgets/personal_table_widget.dart
+++ lib/features/app/presentation/pages/personal/widgets/personal_table_widget.dart
@@ -1,6 +1,10 @@
-import '../../../../domain/entities/person/person.dart';
+import 'dart:developer';
+
 import 'package:data_table_2/data_table_2.dart';
 import 'package:flutter/material.dart';
+
+import '../../../../domain/entities/person/person.dart';
+import '../../../widgets/table_widget.dart';
 
 class PersonalTableWidget extends StatelessWidget {
   final List<PersonEntity> persons;
@@ -39,24 +43,45 @@ class PersonalTableWidget extends StatelessWidget {
     "role",
   ];
 
+  void handleClickOnRow(int rowIndex) {
+    log("Row with id: $rowIndex clicked");
+    // tableRows[rowIndex]
+    clickFunction(rowIndex);
+  }
+
   List<DataRow> _createTableRows(List personal, {int? highlightedRow}) {
     List<DataRow> tableRows = [];
-    for (var i = 0; i < personal.length; i++) {}
+    for (var i = 0; i < personal.length; i++) {
+      tableRows.add(PersonalTableRowMapper.personEntityToRow(
+          personal[i], columnValues, (b) => handleClickOnRow(i)));
+    }
     return tableRows;
   }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    tableRows.addAll(_createTableRows(persons, highlightedRow: highLighted));
+    return TableWidget(columns: tableColumns, rows: tableRows);
   }
 }
 
 class PersonalTableRowMapper {
-  static DataRow? personEntityToRow(
+  static DataRow personEntityToRow(
       PersonEntity model, List<String> columns, void Function(bool?)? onTap,
       {bool isHighLighted = false}) {
     List<DataCell> cells = List<DataCell>.empty(growable: true);
-    // Map<String, dynamic> actionJSON = model.toJson();
-    return null;
+    Map<String, dynamic> personJSON = model.toMap();
+    for (var columnValue in columns) {
+      if (personJSON.containsKey(columnValue) == false) {
+        throw "PersonEntity object doesnt contains key:'$columnValue'";
+      }
+      cells.add(DataCell(Text(personJSON[columnValue])));
+    }
+
+    DataRow row = DataRow(
+        onSelectChanged: (onTap != null) ? onTap : null,
+        cells: cells,
+        selected: isHighLighted);
+    return row;
   }
 }
