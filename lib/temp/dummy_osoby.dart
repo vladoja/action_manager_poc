fcadffcd6a344ce72fc1377e09cf6fe082085ad8
vladoja <dr.jalo2@gmail.com>
2023-12-20 16:25:23 +0100

    OsobaEntity dummy data

---
+++ lib/temp/dummy_osoby.dart
@@ -0,0 +1,20 @@
+import '../features/app/domain/entities/osoba/osoba.dart';
+
+const zoznamy_osoby_dummy = [
+  OsobaEntity(
+      id: 1,
+      firstName: "Martin",
+      lastName: "Dúbravka",
+      title: "glkp",
+      phone: "+421123321",
+      city: "Newcastle",
+      employer: "NUFC"),
+  OsobaEntity(
+      id: 2,
+      firstName: "Milan",
+      lastName: "Škriniar",
+      title: "cb",
+      phone: "+421789159",
+      city: "Paríž",
+      employer: "Paris Stg."),
+];
