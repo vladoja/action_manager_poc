f88ac81caf2d8a47f19f94c576e69c1cf316dbeb
vladoja <dr.jalo2@gmail.com>
2024-01-17 13:43:17 +0100

    util: parseDotValue() with unit tests

--- lib/core/utils/action_utils.dart
+++ lib/core/utils/action_utils.dart
@@ -6,3 +6,38 @@ bool isDate(String str) {
     return false;
   }
 }
+
+dynamic parseDotValue(String pathToValue, Map<String, dynamic> data,
+    {String? sourceNameForErrorLog}) {
+  List<String> keys = pathToValue.split('.');
+  if (keys.length == 1) {
+    String key = keys[0];
+    if (data.containsKey(key)) {
+      return data[key];
+    } else {
+      var sourceName =
+          (sourceNameForErrorLog != null && sourceNameForErrorLog.isNotEmpty)
+              ? '[$sourceNameForErrorLog] '
+              : '';
+      throw Exception("${sourceName}Map object is missing key:'$key'");
+    }
+  } else {
+    dynamic v = data;
+    while (keys.isNotEmpty) {
+      String currentKey = keys.removeAt(0);
+      if (v is Map) {
+        Map<String, dynamic> temp = Map.castFrom(v);
+        if (temp.containsKey(currentKey) == false) {
+          var sourceName = (sourceNameForErrorLog != null &&
+                  sourceNameForErrorLog.isNotEmpty)
+              ? '[$sourceNameForErrorLog] '
+              : '';
+          throw Exception(
+              "${sourceName}Map object is missing key:'$currentKey' from composed key: '$pathToValue");
+        }
+      }
+      v = v[currentKey];
+    }
+    return v;
+  }
+}
