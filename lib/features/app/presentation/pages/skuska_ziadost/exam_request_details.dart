01eea29653d96dcf2fb3c08f48f7b20b6a204577
vladoja <dr.jalo2@gmail.com>
2024-01-22 20:23:02 +0100

    _handlePridajTermin() shows dialog with Exams/Terminy_skusok

--- lib/features/app/presentation/pages/skuska_ziadost/exam_request_details.dart
+++ lib/features/app/presentation/pages/skuska_ziadost/exam_request_details.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../domain/entities/action.dart';
 import '../../../domain/entities/exam/exam_request.dart';
+import '../../bloc/action/action_bloc.dart';
+import '../akcie/widgets/action_table_widget.dart';
 
 class ExamRequestDetails extends StatelessWidget {
   final ExamRequestEntity examRequest;
@@ -36,17 +40,61 @@ class ExamRequestDetails extends StatelessWidget {
             const SizedBox(
               height: 20,
             ),
-            _buildPridelTerminWidget(),
+            _buildPridelTerminWidget(context),
           ],
         ),
       ),
     );
   }
 
-  Widget _buildPridelTerminWidget() {
+  Widget _buildPridelTerminWidget(BuildContext context) {
+    List<ActionEntity> actions = context.read<ActionBloc>().state.actions;
     return ElevatedButton(
-      onPressed: () {},
+      onPressed: () => _handlePridajTermin(context, actions),
       child: const Text('Pridaj termin'),
     );
+  }
+
+  void _handlePridajTermin(
+      BuildContext context, List<ActionEntity> actions) async {
+    List<int> personIdsToAdd = await showDialog(
+      context: context,
+      builder: (context) {
+        List<int> selectedPersonIds = [];
+        return AlertDialog(
+          title: const Text('Vyber termin skusky'),
+          content: ConstrainedBox(
+            constraints: BoxConstraints.expand(
+                width: MediaQuery.of(context).size.width * 0.8,
+                height: MediaQuery.of(context).size.height * 0.9),
+            child: ActionTableWidget(
+              actions: actions,
+              clickFunction: (int id) {
+                // Najviac jeden termin je mozne priradit. Preto: clear()
+                selectedPersonIds.clear();
+                selectedPersonIds.add(actions[id].id);
+              },
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context, <int>[]),
+              child: const Text('Zrušiť'),
+            ),
+            TextButton(
+              onPressed: () => Navigator.pop(context, selectedPersonIds),
+              child: const Text('Pridať'),
+            ),
+          ],
+        );
+      },
+    );
+    if (personIdsToAdd.isNotEmpty) {
+      // print('Pridany personal: $personIdsToAdd');
+      if (!context.mounted) return;
+      // _emitAddPersonalToActionEvent(context, personIdsToAdd, persons, action);
+    } else {
+      // print('Nikto nebol pridany');
+    }
   }
 }
