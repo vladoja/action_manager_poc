82bae255418c1985c5cf7711b9a90c85a625e628
vladoja <dr.jalo2@gmail.com>
2024-02-08 17:59:59 +0100

    TextField to TextFormField because of the validation. Min 2 characters.

--- lib/features/app/presentation/pages/oso/widgets/search_oso_widget.dart
+++ lib/features/app/presentation/pages/oso/widgets/search_oso_widget.dart
@@ -5,24 +5,45 @@ class SearchOsoWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final GlobalKey<FormState> formKey = GlobalKey<FormState>();
     return Column(
       children: [
-        TextField(
-          decoration: const InputDecoration(
-            labelText: 'Vyhladaj ...',
-            border: InputBorder.none,
-            filled: false,
-            prefixIcon: Icon(Icons.search),
+        Form(
+          key: formKey,
+          autovalidateMode: AutovalidateMode.onUserInteraction,
+          child: TextFormField(
+            validator: (value) {
+              if (value == null || getCleanedText(value).length < 2) {
+                return 'Zadaj minimalne 2 znaky';
+              }
+              if (value != getCleanedText(value)) {
+                return 'Retazec obsahuje nepovolene znaky';
+              }
+              return null;
+            },
+            decoration: const InputDecoration(
+              labelText: 'Vyhladaj ...',
+              border: InputBorder.none,
+              filled: false,
+              prefixIcon: Icon(Icons.search),
+            ),
+            onChanged: (String? newSearchTerm) {
+              if (newSearchTerm != null && newSearchTerm.length > 1) {
+                String searchedTerm = getCleanedText(newSearchTerm);
+                debugPrint('Searching for: "$searchedTerm"');
+              }
+              // else {
+              //   // formKey.currentState!.validate();
+              //   debugPrint('Search. At least 2 characters');
+              // }
+            },
           ),
-          onChanged: (String? newSearchTerm) {
-            if (newSearchTerm != null && newSearchTerm.length > 1) {
-              debugPrint('Searching for: "${newSearchTerm.toString()}"');
-            } else {
-              debugPrint('Search. At least 2 characters');
-            }
-          },
         )
       ],
     );
   }
+}
+
+String getCleanedText(String text) {
+  return text.replaceAll(RegExp(r"\s+"), '');
 }
